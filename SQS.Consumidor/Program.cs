6928abbb11f81d6afded430ef415da00230e30df
Arthur Mendes <arthur.mendes@compasso.com.br>
2023-06-11 20:10:15 -0300

    Solving SDK AWS showing in the IDE

--- SQS.Consumidor/Program.cs
+++ SQS.Consumidor/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        var client = new Amazon.SQS.Client(RegionEndpoint.USEast1);
+
+        var request = new ReceiveMessageRequest
+        {
+            QueueUrl = "https://sqs.us-west-2.amazonaws.com/517554276447/microservice-test"
+        };
+
+        await client.ReceiveMessageRequest(request);  
+
+
+
+    }
+}
